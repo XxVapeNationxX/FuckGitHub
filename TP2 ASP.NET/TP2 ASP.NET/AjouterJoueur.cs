e2430cb7829bbb741202c4a48dd72518c7a8fe16
XxVapeNationxX <45363501+XxVapeNationxX@users.noreply.github.com>
2019-04-01 23:41:59 -0400

    Add files via upload

---
+++ TP2 ASP.NET/TP2 ASP.NET/AjouterJoueur.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2_ASP.NET
+{
+    public partial class AjouterJoueur : Form
+    {
+        public AjouterJoueur()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TB_Nom.Text) && !string.IsNullOrWhiteSpace(TB_Prenom.Text))
+            {
+                BTN_Ajouter.Enabled = true;
+            }
+            else
+            {
+                BTN_Ajouter.Enabled = false;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TB_Nom.Text) && !string.IsNullOrWhiteSpace(TB_Prenom.Text))
+            {
+                BTN_Ajouter.Enabled = true;
+            }
+            else
+            {
+                BTN_Ajouter.Enabled = false;
+            }
+        }
+
+        private void AjouterJoueur_Load(object sender, EventArgs e)
+        {
+            BTN_Ajouter.Enabled = false;
+        }
+
+        private void BTN_Annuler_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TP2 ASP.NET/TP2 ASP.NET/SupprimerJoueur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2_ASP.NET
+{
+    public partial class SupprimerJoueur : Form
+    {
+        public SupprimerJoueur()
+        {
+            InitializeComponent();
+        }
+
+        private void SupprimerJoueur_Load(object sender, EventArgs e)
+        {
+            BTN_Supprimer.Enabled = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                BTN_Supprimer.Enabled = false;
+            }
+            else
+            {
+                BTN_Supprimer.Enabled = true;
+            }
+        }
+
+        private void BTN_Annuler_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
